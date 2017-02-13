d04e66d5ffe88d34536ffd7f7b384471aa7170ca
John Stradling <jstrad@vt.edu>
2017-02-12 19:56:23 -0500

    Server without pickle load

--- Assignment1/echo_server_exception.py
+++ Assignment1/echo_server_exception.py
@@ -6,8 +6,10 @@ A simple echo server that handles some exceptions
 
 import socket
 import sys
+import wolframalpha
+import pickle
 
-host = ''
+host = 'localhost'
 port = 50000
 backlog = 5
 size = 1024
@@ -22,26 +24,22 @@ except socket.error as message:
         s.close()
     print ("Could not open socket: " + str(message))
     sys.exit(1)
-print('Socket Create')
-try:
-    remote_ip = socket.gethostbyname(host)
-
-except socket.gaierror:
-    # could not resolve
-    print('Hostname could not be resolved. Exiting')
-    sys.exit()
-
-print('Ip address of ' + host + ' is ' + remote_ip)
-
-# Connect to remote server
-s.connect((remote_ip, port))
-
-print
-'Socket Connected to ' + host + ' on ip ' + remote_ip
-
 while 1:
-    client, address = s.accept()
-    data = client.recv(size)
+    wclient, address = s.accept()
+    data = wclient.recv(size)
+    data_loaded = pickle.loads(data)
+
     if data:
-        client.send(data)
-    client.close()
+       # print (data_loaded)
+
+        input = data_loaded
+
+        appid = "27TG8H-VEQ8L3WAJ5"
+        client = wolframalpha.Client(appid)
+
+        res = client.query(input)
+        answer = next(res.results).text
+
+        print(answer)
+        #client.send(data)
+    wclient.close()
