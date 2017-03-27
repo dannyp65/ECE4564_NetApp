54c3183929b5e0a73782a4634386a8780bdb4e0a
Daniel Pham <daniel.pham56@gmail.com>
2017-03-27 16:36:32 -0400

    Add GPIO function

--- Assignment3/icu.py
+++ Assignment3/icu.py
@@ -1,10 +1,19 @@
+#/usr/bin/env python3
 import requests
 import json
 from twilio.rest import TwilioRestClient
 import datetime
+import sys
 import pygame
+import RPi.GPIO as GPIO
 
 now = datetime.datetime.now()
+def get_args():
+    opt_in_opt = "ERROR: cannot take another opt flag as a parameter"
+    cmd_args = sys.argv
+    cmd_len = len(cmd_args)
+    for i in range(1, cmd_len):
+
 
 def playASound():
     pygame.mixer.init()
@@ -12,6 +21,7 @@ def playASound():
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy() == True:
       continue
+
 def sendMess(mess, num):
     tonum = '+1' + num
     twilio_sid = "AC3fc819a1fe2f836b25e54e0ef47ba23d"
@@ -19,8 +29,6 @@ def sendMess(mess, num):
     client = TwilioRestClient(twilio_sid, twilio_token)
     message = client.messages.create(to=tonum, from_="+18045523194",
                                      body=mess)
-    print(message)
-    #return message
 
 def get_OWM(zipcode):
     cloud_list = []
@@ -33,6 +41,7 @@ def get_OWM(zipcode):
     for i in range(0,16):
         cloud_list.append(weather_list[i]['clouds'])
     return cloud_list
+
 def displayWeather(cloud):
     today = now = datetime.datetime.now()
     date = today.day
@@ -43,9 +52,24 @@ def displayWeather(cloud):
         print(': ', cloud[i], '%', '\t', sep= '', end='')
         if i == 7:
             print('\n')
+def init_LED():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.setup(23, GPIO.OUT)
+    GPIO.setup(24, GPIO.OUT)
+
+def control_LED(control):
+    if control == 'on':
+        GPIO.output(23, GPIO.HIGH)
+        GPIO.output(24, GPIO.HIGH)
+        GPIO.output(18, GPIO.LOW)
+    else:
+        GPIO.output(24, GPIO.LOW)
+        GPIO.output(18, GPIO.LOW)
+        GPIO.output(23, GPIO.LOW)
 
 
 x = get_OWM('24060')
-#print(x)
 displayWeather(x)
 playASound()
