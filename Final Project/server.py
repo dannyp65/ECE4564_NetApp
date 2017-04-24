82b5405046f8a83cacdd6b653a49fdb193e45f92
momesmo <momesmo@vt.edu>
2017-04-24 16:27:20 +0000

    commandline back to a, c, s, r

--- Final Project/server.py
+++ Final Project/server.py
@@ -8,61 +8,73 @@ import json
 
 # Function for command line arguments
 def get_args():
-    opt_in_opt = "ERROR: cannot take another opt flag as a parameter"
-    cmd_args = sys.argv
-    cmd_len = len(cmd_args)
-    a_check, z_check, r_check = False, False, False
-    a_opt, z_opt, r_opt = '', '', ''
-    for i in range(1, cmd_len):
-        if cmd_args[i] == a_opt or cmd_args[i] == z_opt or cmd_args[i] == r_opt:
-            continue
-        if cmd_args[i] == '-a':
-            try:
-                a_opt = cmd_args[i + 1]  # raise out of range
-                if a_opt == '-a' or a_opt == '-z' or a_opt == '-r':
-                    print(opt_in_opt, '1')
-                    sys.exit()  # raise SystemExit
-            except SystemExit:
-                raise SystemExit
-            except:
-                print("ERROR: no Activity given")
-                raise SystemExit
-            a_check = True
-        elif cmd_args[i] == '-z':
-            try:
-                z_opt = cmd_args[i + 1]
-                if z_opt == '-a' or z_opt == '-z' or z_opt == '-r':
-                    print(opt_in_opt, '3')
-                    sys.exit()
-            except SystemExit:
-                raise SystemExit
-            except:
-                print("ERROR: no Zipcode given")
-                raise SystemExit
-            z_check = True
-        elif cmd_args[i] == '-r':
-            try:
-                r_opt = cmd_args[i + 1]
-                if r_opt == '-a' or r_opt == '-z' or r_opt == '-r':
-                    print(opt_in_opt, '4')
-                    sys.exit()
-            except SystemExit:
-                raise SystemExit
-            except:
-                print("ERROR: no Radius given")
-                raise SystemExit
-            r_check = True
-        elif cmd_args[i].startswith('-'):
-            print("ERROR: incorrect opt flag")
-            sys.exit()
-        else:
-            print("ERROR: ELSE", cmd_args[i])
-            sys.exit()
-    if a_check and z_check and r_check:
-        return a_opt, z_opt, r_opt
-    else:
-        print("ERROR: all parameters need to be set")
-        sys.exit()
+     opt_in_opt = "ERROR: cannot take another opt flag as a parameter"
+     cmd_args = sys.argv
+     cmd_len = len(cmd_args)
+     a_check, c_check, s_check, r_check = False, False, False, False
+     a_opt, c_opt, s_opt, r_opt = '', '', '', ''
+     for i in range(1, cmd_len):
+         if cmd_args[i] == a_opt or cmd_args[i] == c_opt or cmd_args[i] == s_opt or cmd_args[i] == r_opt:
+             continue
+         if cmd_args[i] == '-a':
+             try:
+                 a_opt = cmd_args[i + 1]  # raise out of range
+                 if a_opt == '-a' or a_opt == '-c' or a_opt == '-s' or a_opt == '-r':
+                     print(opt_in_opt, '1')
+                     sys.exit()  # raise SystemExit
+             except SystemExit:
+                 raise SystemExit
+             except:
+                 print("ERROR: no Activity given")
+                 raise SystemExit
+             a_check = True
+         elif cmd_args[i] == '-s':
+             try:
+                 s_opt = cmd_args[i + 1]
+                 if s_opt == '-a' or s_opt == '-c' or s_opt == '-s' or s_opt == '-r':
+                     print(opt_in_opt, '2')
+                     sys.exit()
+             except SystemExit:
+                 raise SystemExit
+             except:
+                 print("ERROR: no State given")
+                 raise SystemExit
+             s_check = True
+         elif cmd_args[i] == '-c':
+             try:
+                 c_opt = cmd_args[i + 1]
+                 if c_opt == '-a' or c_opt == '-c' or c_opt == '-s' or c_opt == '-r':
+                     print(opt_in_opt, '3')
+                     sys.exit()
+             except SystemExit:
+                 raise SystemExit
+             except:
+                 print("ERROR: no City given")
+                 raise SystemExit
+             c_check = True
+         elif cmd_args[i] == '-r':
+             try:
+                 r_opt = cmd_args[i + 1]
+                 if r_opt == '-a' or r_opt == '-c' or r_opt == '-s' or r_opt == '-r':
+                     print(opt_in_opt, '4')
+                     sys.exit()
+             except SystemExit:
+                 raise SystemExit
+             except:
+                 print("ERROR: no Radius given")
+                 raise SystemExit
+             r_check = True
+         elif cmd_args[i].startswith('-'):
+             print("ERROR: incorrect opt flag")
+             sys.exit()
+         else:
+             print("ERROR: ELSE", cmd_args[i])
+             sys.exit()
+     if a_check and c_check and s_check and r_check:
+         return a_opt, c_opt, s_opt, r_opt
+     else:
+         print("ERROR: all parameters need to be set")
+         sys.exit()
 
 def get_OWM(zipcode):
     cloud_list = []
@@ -85,10 +97,10 @@ def get_OWM(zipcode):
     except:
         print('Error: Invalid response from weather API')
         sys.exit()
-def get_Trail(activity, lat, long, radius):
-    #full_api_url = 'https://trailapi-trailapi.p.mashape.com/?lon=-&q[activities_activity_type_name_eq]=' + activity + '&q[city_cont]=' + city + '&q[country_cont]=United+State&q[state_cont]=' + state +'&radius=' +radius
-    api_url = 'https://trailapi-trailapi.p.mashape.com/?lat=' + lat + '&lon=' + long + '&q[activities_activity_type_name_eq]=' + activity +' &radius=' + radius
-    response = requests.get(api_url, headers={ "X-Mashape-Key": "kopJS5O41PmshhVPxUeXCkLj8rOQp14geTqjsnSiGdq8SoUTWR", "Accept": "tapplication/json"})
+def get_Trail(activity, city, state, radius):
+    full_api_url = 'https://trailapi-trailapi.p.mashape.com/?lon=-&q[activities_activity_type_name_eq]=' + activity + '&q[city_cont]=' + city + '&q[country_cont]=United+State&q[state_cont]=' + state +'&radius=' +radius
+    #api_url = 'https://trailapi-trailapi.p.mashape.com/?lat=' + lat + '&lon=' + long + '&q[activities_activity_type_name_eq]=' + activity +' &radius=' + radius
+    response = requests.get(full_api_url, headers={ "X-Mashape-Key": "kopJS5O41PmshhVPxUeXCkLj8rOQp14geTqjsnSiGdq8SoUTWR", "Accept": "tapplication/json"})
     return json.loads(response.text)
 
 def main():       
@@ -98,13 +110,13 @@ def main():
 	size = 1024
 	s = None
 	
-	activity, zipcode, radius = get_args()
-	print(activity, zipcode, radius)
+	activity, city, state, radius = get_args()
+	print(activity, city, state, radius)
 	#example of using trail API with activity, city, state, and radius
 	# there are only 2 types of activites:  
 	#to-do: parse zipcode to city and state
 	trail_data = get_Trail(activity, city, state, radius)
-	print (trail_data[0], trail_data[1])
+	print (trail_data)
 	try:
 	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object that use IPv4 and TCP
 	    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
