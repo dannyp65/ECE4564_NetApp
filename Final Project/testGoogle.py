5f5a7048a80048f1e42b115d57f91681a85e53d9
John Stradling <jstrad@vt.edu>
2017-05-08 02:59:59 +0000

    Testing Google API

--- Final Project/testGoogle.py
+++ Final Project/testGoogle.py
@@ -7,14 +7,17 @@ import zipcode
 import json
 
 def get_Google(activity, zipcode, radius):
-    full_api_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query='+activity+zipcode+'&radius='+radius+'&key=AlzaSyCeje-BewlHbBDwXp5sL18nE81puhBNMHo'
-    # api_url = 'https://trailapi-trailapi.p.mashape.com/?lat=' + lat + '&lon=' + long + '&q[activities_activity_type_name_eq]=' + activity +' &radius=' + radius
+    full_api_url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query='+activity+zipcode+'&radius='+radius+'&key=AIzaSyCXSbe5xRL_aa-Op0AVNwJ-RsZgcCmvPSY'
     response = requests.get(full_api_url)
     return json.loads(response.text)
 
 def main():
     activity = "hiking"
     zipcode = "24060"
-    radius = "50000"
-    print(get_Goolge(activity, zipcode, radius))
+    radius = "5"
+    final_result = get_Google(activity, zipcode, radius)
+    print (final_result)
+
+if __name__ == "__main__":
+    main()
     
