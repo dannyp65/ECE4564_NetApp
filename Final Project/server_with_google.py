d44a2d35c4386b914dd65f919a03de6335360564
yoken444 <yoken@vt.edu>
2017-05-09 16:20:01 +0000

    pretty much finished server

--- Final Project/server_with_google.py
+++ Final Project/server_with_google.py
@@ -8,7 +8,7 @@ import zipcode
 import json
 import pickle
 import hashlib
-
+import RPi.GPIO as GPIO
 
 # this function generates a MD5 checksum value from string
 def MD5_Encode(str):
@@ -47,13 +47,49 @@ def get_Google(activity, city, state, radius):
     response = requests.get(full_api_url)
     return json.loads(response.text)
 
+# calculates the thresholds and enables the GPIO pins to light up LED
+def LEDLight(cloud):
+    #turn off all LEDs
+    GPIO.output(24,GPIO.LOW)
+    GPIO.output(18,GPIO.LOW)
+    GPIO.output(23,GPIO.LOW)
+    if cloud < 25:
+        #turn on green
+        print('LED is green\n')
+        GPIO.output(24, GPIO.HIGH)
+        GPIO.output(18,GPIO.LOW)
+        GPIO.output(23,GPIO.LOW)
+    elif cloud > 70:
+        #turn on red
+        print('LED is red\n')
+        GPIO.output(23, GPIO.HIGH)
+        GPIO.output(18,GPIO.LOW)
+        GPIO.output(24,GPIO.LOW)
+    else:
+        #turn on yellow = green + red
+        print('LED is yellow\n')
+        GPIO.output(23, GPIO.HIGH)
+        GPIO.output(24, GPIO.HIGH)
+        GPIO.output(18,GPIO.LOW)  
+        
+def init_LED():
+    print("Setting up GPIO pins for the LED")
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+    GPIO.setup(18,GPIO.OUT)
+    GPIO.setup(23,GPIO.OUT)
+    GPIO.setup(24,GPIO.OUT)
+    GPIO.output(24,GPIO.LOW)
+    GPIO.output(18,GPIO.LOW)
+    GPIO.output(23,GPIO.LOW)
+
 def main():
     host = ''
     port = 1234
     backlog = 5
     size = 4096
     s = None
-
+    init_LED()
     # socket connection and information transition
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Create a socket object that use IPv4 and TCP
@@ -82,21 +118,23 @@ def main():
             city = params.split(',')[1]
             state = params.split(',')[2]
             radius = params.split(',')[3]
+            newradius = int(radius) * 1609
+            print(newradius)
             print("Parameters Received From Client:", activity, city, state, radius)
             # example of using trail API with activity, city, state, and radius
             # there are only 2 types of activites:
             places = []
-            trail_data = get_Google(activity, city, state, radius)
+            trail_data = get_Google(activity, city, state, str(newradius))
             # stores location's information from JSON dictionary in list
             for loc in trail_data["results"]:
                 if (loc.get("rating")):
                     if (loc.get("photos")):
-                        places.append([loc["name"],loc["formatted_address"],loc["rating"], loc["photos"]])
+                        places.append([loc["name"],loc["formatted_address"],loc["rating"], loc["photos"][0]["photo_reference"]])
                     else:
                         places.append([loc["name"],loc["formatted_address"],loc["rating"]])
                 else:
                     if (loc.get("photos")):
-                        places.append([loc["name"],loc["formatted_address"],loc["photos"]])
+                        places.append([loc["name"],loc["formatted_address"],loc["photos"][0]["photo_reference"]])
                     else:
                         places.append([loc["name"],loc["formatted_address"]])
             # if nothing is found add a message to the list
@@ -105,12 +143,11 @@ def main():
 
             weather = get_OWM(city, state)
             weather_stats = [weather["main"]["temp"], weather["main"]["temp_min"], weather["main"]["temp_max"],
-                             weather["wind"]["speed"], weather["clouds"]["all"]]
-
+                             weather["wind"]["speed"], weather["clouds"]["all"], weather["weather"][0]["description"]]
+            LEDLight(weather_stats[4])
             str_places = '[!]'.join(str(x) for x in places)
             str_weather = '[!]'.join(str(y) for y in weather_stats)
             str_send = str_places + '[?]' + str_weather
-            #print(str_send)
             send_checksum = MD5_Encode(str_send)
             tuple_send = (str_send, send_checksum)
             pickle_send = pickle.dumps(tuple_send)
